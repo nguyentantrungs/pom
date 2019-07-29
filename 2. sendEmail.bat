ec89b2bfc0abb4b14710af7f5ec6f7400e7dd3d4
nguyentantrungs <nguyentantrungs@gmail.com>
2019-07-29 15:47:01 +0700

    fix deleting files and subfolder in temp report folder

--- 2. sendEmail.bat
+++ 2. sendEmail.bat
@@ -1,5 +1,5 @@
 cd C:\Test\Report\temp
-DEL /Q *
+DEL /S /Q *
 xcopy C:\Test\DAClient\target /E /Y
 cd C:\Test\DAClient
 mvn install -Dtest=testrunner -Dmaven.test.failuer.ignore=true -Dcucumber.options="src/test/java/features/send_email_report.feature"
